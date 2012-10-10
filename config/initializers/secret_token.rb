c773d12575987f016401d87f096200b07188c4a3
Clayton Rogers <clayton.rogers@gmail.com>
2012-10-10 15:53:02 -0500

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AzureTest::Application.config.secret_token = '5f7bdc089f5e53dfa0c334548b14f5abe5801b912bfa127ba4bbf7a40b92b4524acb2de328f6e7c7ff06bfbac99652415b16a485ab61f4cb1a1db1f247cca54f'
